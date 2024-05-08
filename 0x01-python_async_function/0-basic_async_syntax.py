e3f648acd0efa051fbe14292911176a84987fafd
TRANDY-116 <randyfun16@gmail.com>
2024-05-08 12:20:55 +0100

    pycodestyle for 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" 
+"""
 Async coroutine that takes in an interger, waits for random
-delay(0 -10) and then returns it 
+delay(0 -10) and then returns it
 """
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Waits for a random delay between 0 and the max delay"""
