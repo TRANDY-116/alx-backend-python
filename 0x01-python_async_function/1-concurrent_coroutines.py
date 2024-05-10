ca27d2dcabb27fd67141e72a6b769af7db9a63c8
TRANDY-116 <randyfun16@gmail.com>
2024-05-10 20:14:28 +0100

    fixed the list output delcaration

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,7 +15,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> [float]:
     """ Spawns wait_Return n times with the specified max_delay """
     # Create a list of coroutine objects using a generator expression
     coroutines = (wait_random(max_delay) for _ in range(n))
